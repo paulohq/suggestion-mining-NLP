3d053327ab52e44e6488995686625db2be4b5847
= <=>
2018-12-03 11:58:30 -0200

    Correção do método remove_characters_after_tokenization. Criação dos métodos lower_case, remove_stopwords e remove_repeated_characters.

--- exemplos.py
+++ exemplos.py
@@ -2,12 +2,18 @@ import nltk
 import re
 import string
 from pprint import pprint
+from nltk.corpus import wordnet
 
 
-corpus = ["The brown fox wasn't that quick and he couldn't win the race",
-"Hey that's a great deal! I just bought a phone for $199",
-"@@You'll (learn) a **lot** in the book. Python is an amazing language !@@"]
+corpus = ["The brown fox wasn't that quick and he couldn't win the race.''",
+"Hey that's a great deal! I just not bought a phone for $199 ",
+"@@You'll (learn) a **lot** in the book. Python is an amazinngggg language !@@",
+"My schooool is realllllyyy amaaazingggg"]
 
+filtered_list = []
+filtered_list_lowercase = []
+filtered_list_remove_stopwords = []
+filtered_list_remove_repeated_characters = []
 
 def tokenize_text(text):
     sentences = nltk.sent_tokenize(text)
@@ -16,11 +22,33 @@ def tokenize_text(text):
 
 def remove_characters_after_tokenization(tokens):
     pattern = re.compile('[{}]'.format(re.escape(string.punctuation)))
-    for token in tokens:
-        filtered_tokens = list(filter(None, [pattern.sub('', token) ]))
-    #filtered_tokens = list(filter(None, [pattern.sub('', token) for token in tokens]))
+    #for token in tokens:
+    #    filtered_tokens = list(filter(None, [pattern.sub('', token) ]))
+    filtered_tokens = list(filter(None, [pattern.sub('', token) for token in tokens]))
     #print (filtered_tokens)
     return filtered_tokens
+
+def lower_case(token_list):
+    lower_token_list = [token.lower() for token in token_list]
+    return lower_token_list
+
+def remove_stopwords(tokens):
+    stopword_list = nltk.corpus.stopwords.words('english')
+    filtered_tokens = [token for token in tokens if token not in stopword_list]
+    return filtered_tokens
+
+def remove_repeated_characters(tokens):
+    repeat_pattern = re.compile(r'(\w*)(\w)\2(\w*)')
+    match_substitution = r'\1\2\3'
+    def replace(old_word):
+        if wordnet.synsets(old_word):
+            return old_word
+
+        new_word = repeat_pattern.sub(match_substitution, old_word)
+        return replace(new_word) if new_word != old_word else new_word
+
+    correct_tokens = [replace(word) for word in tokens]
+    return correct_tokens
 
 token_list = [tokenize_text(text) for text in corpus]
 
@@ -30,7 +58,31 @@ pprint(token_list)
 #                                for tokens in sentence_tokens]
 #                                    for sentence_tokens in token_list]
 
-filtered_list_1 = list([filter(None,[remove_characters_after_tokenization(tokens)
-                                for tokens in sentence_tokens])
-                                    for sentence_tokens in token_list])
-print (filtered_list_1)
+#Loop to remove special characters from the list token_list.
+for sentence_tokens in token_list:
+    for tokens in sentence_tokens:
+        filtered_list.append(list(filter(None, [remove_characters_after_tokenization(tokens)])))
+#Print list after remove special characters.
+print(filtered_list)
+
+#Loop to convert words in filtered_list to lowercase.
+for sentence_tokens in filtered_list:
+    for tokens in sentence_tokens:
+        filtered_list_lowercase.append(list(filter(None, [lower_case(tokens)])))
+#Print list after convert to lowercase.
+print(filtered_list_lowercase)
+
+#Loop to remove the stopwords from the list filtered_list_lowercase
+for sentence_tokens in filtered_list_lowercase:
+    for tokens in sentence_tokens:
+        filtered_list_remove_stopwords.append(list(filter(None, [remove_stopwords(tokens)])))
+
+#Print list after remove stopwords
+print (filtered_list_remove_stopwords)
+
+#Loop to remove the repeated characters from the list filtered_list_remove_stopwords.
+for sentence_tokens in filtered_list_remove_stopwords:
+    for tokens in sentence_tokens:
+        filtered_list_remove_repeated_characters.append(list(filter(None, [remove_repeated_characters(tokens)])))
+#Print list after remove repeated characters.
+print(filtered_list_remove_repeated_characters)
--- text-classification-embedding.py
+++ text-classification-embedding.py
@@ -6,12 +6,19 @@ import nltk
 from pprint import pprint
 import re
 import string
+import tkinter
+from nltk.corpus import wordnet
 
 #train = file_reader = csv.reader(open(data_path, "rt", errors="ignore", encoding="utf-8"), delimiter=',')
 data_path = "/home/paulo/PycharmProjects/suggestion-mining/training-full-v13-bkp.csv"
 #train = pd.read_csv(data_path, header=0, sep=";", quotechar='"',quoting=3, encoding='utf-8')
 
-# This reads CSV a given CSV and stores the data in a list
+filtered_list = []
+filtered_list_lowercase = []
+filtered_list_remove_stopwords = []
+filtered_list_remove_repeated_characters = []
+
+# Reads a given CSV and stores the data in a list
 def read_csv(data_path):
     file_reader = csv.reader(open(data_path, "rt", errors="ignore", encoding="utf-8"), delimiter=',')
     sent_list = []
@@ -24,6 +31,7 @@ def read_csv(data_path):
         sent_list.append((id, suggest, sent))
     return sent_list
 
+#Tokenize the text into sentences
 def sent_tokenize(sent_list):
     default_st = nltk.sent_tokenize
     for sent in sent_list:
@@ -36,27 +44,86 @@ def word_tokenize1(sent_list):
         token_list = [default_w(sent[1])]
         print(token_list[0])
 
+#Tokenize the sentence into words.
 def word_tokenize(sentence):
     default_w = nltk.word_tokenize
     word_tokens = [default_w(sentence)]
     return word_tokens
 
+# Remove special characters after tokenization
 def remove_characters_after_tokenization(token_list):
     pattern = re.compile('[{}]'.format(re.escape(string.punctuation)))
-    for tokens in token_list:
-        for token in tokens:
-            filtered_tokens = list([filter(None, [pattern.sub('', token)])])
-    #filtered_tokens = filter(None, [pattern.sub('', token) for token in token_list])
+    #for token in token_list:
+    #    filtered_tokens = list(filter(None, [pattern.sub('', token) ]))
+
+#    for tokens in token_list:
+#        for token in tokens:
+#            filtered_tokens = list([filter(None, [pattern.sub('', token)])])
+    filtered_tokens = list(filter(None, [pattern.sub('', token) for token in token_list]))
+    #print(filtered_tokens)
+
     return filtered_tokens
+
+#Convert tokens to lowercase.
+def lower_case(token_list):
+    lower_token_list = [token.lower() for token in token_list]
+    return lower_token_list
+
+#Remove the stopwords from the token list.
+def remove_stopwords(tokens):
+    stopword_list = nltk.corpus.stopwords.words('english')
+    filtered_tokens = [token for token in tokens if token not in stopword_list]
+    return filtered_tokens
+
+#Remove the repeated characters from the token list.
+def remove_repeated_characters(tokens):
+    repeat_pattern = re.compile(r'(\w*)(\w)\2(\w*)')
+    match_substitution = r'\1\2\3'
+    def replace(old_word):
+        if wordnet.synsets(old_word):
+            return old_word
+
+        new_word = repeat_pattern.sub(match_substitution, old_word)
+        return replace(new_word) if new_word != old_word else new_word
+
+    correct_tokens = [replace(word) for word in tokens]
+    return correct_tokens
 
 sent_list = read_csv(data_path)
 #sent_tokenize(sent_list)
 #word_tokenize(sent_list)
 token_list = [word_tokenize(sent[1]) for sent in sent_list]
-pprint(token_list)
+#Print list after execute word tokenize.
+#pprint(token_list)
 
-filtered_list_1 = remove_characters_after_tokenization(token_list)
-#filtered_list_1 = [filter(None,[remove_characters_after_tokenization(tokens)
-#                                for tokens in sentence_tokens])
-#                            for sentence_tokens in token_list]
-print(filtered_list_1)
+#Loop to remove special characters from the list token_list.
+for sentence_tokens in token_list:
+    for tokens in sentence_tokens:
+        filtered_list.append(list(filter(None, [remove_characters_after_tokenization(tokens)])))
+#Print list after remove special characters.
+print(filtered_list)
+
+#Loop to convert words in filtered_list to lowercase.
+for sentence_tokens in filtered_list:
+    for tokens in sentence_tokens:
+        filtered_list_lowercase.append(list(filter(None, [lower_case(tokens)])))
+#Print list after convert to lowercase.
+print(filtered_list_lowercase)
+
+#Loop to remove the stopwords from the list filtered_list_lowercase
+for sentence_tokens in filtered_list_lowercase:
+    for tokens in sentence_tokens:
+        filtered_list_remove_stopwords.append(list(filter(None, [remove_stopwords(tokens)])))
+
+#Print list after remove stopwords
+print (filtered_list_remove_stopwords)
+
+#Loop to remove the repeated characters from the list filtered_list_remove_stopwords.
+for sentence_tokens in filtered_list_remove_stopwords:
+    for tokens in sentence_tokens:
+        filtered_list_remove_repeated_characters.append(list(filter(None, [remove_repeated_characters(tokens)])))
+#Print list after remove repeated characters.
+for sentence_tokens in filtered_list_remove_repeated_characters:
+    for tokens in sentence_tokens:
+      print(tokens)
+
