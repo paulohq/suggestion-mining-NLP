db53a5efc86bddce1f8c2bcbdf9fdf46c6a24fd3
= <=>
2018-12-06 12:11:12 -0200

    Criação de classe no text-classification-embedding. Inclusão do __init__.py para importar os módulos de outros arquivos.

--- exemplos.py
+++ exemplos.py
@@ -6,145 +6,149 @@ from nltk.corpus import wordnet
 from nltk.stem import LancasterStemmer
 from nltk.stem import WordNetLemmatizer # lemmatizes word based on it's parts of speech
 from nltk import pos_tag, word_tokenize
-
+import collections
 from collections import Counter
+from corretor_ortografico_norvig import *
 
 from nltk.corpus import wordnet # To get words in dictionary with their parts of speech
 
 
+class exemplo(object):
+    def __init__(self):
+        self.corpus = ["The brown fox wasn't that quick and he couldn't win the race.''",
+                    "Hey that's a great deal! I just not bought a phone for $199 ",
+                    "@@You'll (learn) a **lot** in the book. Python is an amazinngggg language !@@",
+                    "My schooool is realllllyyy amaaazingggg"]
 
-corpus = ["The brown fox wasn't that quick and he couldn't win the race.''",
-"Hey that's a great deal! I just not bought a phone for $199 ",
-"@@You'll (learn) a **lot** in the book. Python is an amazinngggg language !@@",
-"My schooool is realllllyyy amaaazingggg"]
+        self.token_list = []
+        self.filtered_list = []
+        self.filtered_list_lowercase = []
+        self.filtered_list_remove_stopwords = []
+        self.filtered_list_remove_repeated_characters = []
+        self.filtered_list_stemmer = []
+        self.filtered_list_lemma = []
 
-filtered_list = []
-filtered_list_lowercase = []
-filtered_list_remove_stopwords = []
-filtered_list_remove_repeated_characters = []
-filtered_list_stemmer = []
-filtered_list_lemma = []
+    def tokenize_text(self,text):
+        sentences = nltk.sent_tokenize(text)
+        word_tokens = [nltk.word_tokenize(sentence) for sentence in sentences]
+        return word_tokens
 
-def tokenize_text(text):
-    sentences = nltk.sent_tokenize(text)
-    word_tokens = [nltk.word_tokenize(sentence) for sentence in sentences]
-    return word_tokens
+    def remove_characters_after_tokenization(self,tokens):
+        pattern = re.compile('[{}]'.format(re.escape(string.punctuation)))
+        #for token in tokens:
+        #    filtered_tokens = list(filter(None, [pattern.sub('', token) ]))
+        filtered_tokens = list(filter(None, [pattern.sub('', token) for token in tokens]))
+        #print (filtered_tokens)
+        return filtered_tokens
 
-def remove_characters_after_tokenization(tokens):
-    pattern = re.compile('[{}]'.format(re.escape(string.punctuation)))
-    #for token in tokens:
-    #    filtered_tokens = list(filter(None, [pattern.sub('', token) ]))
-    filtered_tokens = list(filter(None, [pattern.sub('', token) for token in tokens]))
-    #print (filtered_tokens)
-    return filtered_tokens
+    def lower_case(self, token_list):
+        lower_token_list = [token.lower() for token in token_list]
+        return lower_token_list
 
-def lower_case(token_list):
-    lower_token_list = [token.lower() for token in token_list]
-    return lower_token_list
+    def remove_stopwords(self, tokens):
+        stopword_list = nltk.corpus.stopwords.words('english')
+        filtered_tokens = [token for token in tokens if token not in stopword_list]
+        return filtered_tokens
 
-def remove_stopwords(tokens):
-    stopword_list = nltk.corpus.stopwords.words('english')
-    filtered_tokens = [token for token in tokens if token not in stopword_list]
-    return filtered_tokens
+    def remove_repeated_characters(self, tokens):
+        repeat_pattern = re.compile(r'(\w*)(\w)\2(\w*)')
+        match_substitution = r'\1\2\3'
+        def replace(old_word):
+            if wordnet.synsets(old_word):
+                return old_word
 
-def remove_repeated_characters(tokens):
-    repeat_pattern = re.compile(r'(\w*)(\w)\2(\w*)')
-    match_substitution = r'\1\2\3'
-    def replace(old_word):
-        if wordnet.synsets(old_word):
-            return old_word
+            new_word = repeat_pattern.sub(match_substitution, old_word)
+            return replace(new_word) if new_word != old_word else new_word
 
-        new_word = repeat_pattern.sub(match_substitution, old_word)
-        return replace(new_word) if new_word != old_word else new_word
+        correct_tokens = [replace(word) for word in tokens]
+        return correct_tokens
 
-    correct_tokens = [replace(word) for word in tokens]
-    return correct_tokens
-
-def lancaster_stemmer(tokens):
-    ls = LancasterStemmer()
-    filtered_tokens = [ls.stem(token) for token in tokens]
-    return filtered_tokens
-
-
-def get_pos(word):
-    w_synsets = wordnet.synsets(word)
-
-    pos_counts = Counter()
-    pos_counts["n"] = len([item for item in w_synsets if item.pos() == "n"])
-    pos_counts["v"] = len([item for item in w_synsets if item.pos() == "v"])
-    pos_counts["a"] = len([item for item in w_synsets if item.pos() == "a"])
-    pos_counts["r"] = len([item for item in w_synsets if item.pos() == "r"])
-
-    most_common_pos_list = pos_counts.most_common(3)
-    return most_common_pos_list[0][0]  # first indexer for getting the top POS from list, second indexer for getting POS from tuple( POS: count )
+    def lancaster_stemmer(self, tokens):
+        ls = LancasterStemmer()
+        filtered_tokens = [ls.stem(token) for token in tokens]
+        return filtered_tokens
 
 
-#Convert the tokens to your radical (root word) using tokens POS-tag before lemmatizing.
-def lemmatizer(tokens):
-    wnl = WordNetLemmatizer()
+    def get_pos(self, word):
+        w_synsets = wordnet.synsets(word)
 
-    def POS_tag(token):
-        tag = pos_tag(token)
-        wntag = tag[0].lower()
-        wntag = wntag if wntag in ['a', 'r', 'n', 'v'] else None
-        return wntag
+        pos_counts = Counter()
+        pos_counts["n"] = len([item for item in w_synsets if item.pos() == "n"])
+        pos_counts["v"] = len([item for item in w_synsets if item.pos() == "v"])
+        pos_counts["a"] = len([item for item in w_synsets if item.pos() == "a"])
+        pos_counts["r"] = len([item for item in w_synsets if item.pos() == "r"])
 
-    #for token, tag in pos_tag(tokens):
-    #    wntag = tag[0].lower()
-    #    wntag = wntag if wntag in ['a', 'r', 'n', 'v'] else None
-        #lemma = wnl.lemmatize(token, wntag) if wntag else token
+        most_common_pos_list = pos_counts.most_common(3)
+        return most_common_pos_list[0][0]  # first indexer for getting the top POS from list, second indexer for getting POS from tuple( POS: count )
 
-    lemma_tokens = [wnl.lemmatize(token, get_pos(token)) for token in tokens]
 
-    #lemma_tokens = [wnl.lemmatize(token, POS_tag(token)) if POS_tag(token) else token for token in tokens]
-    return lemma_tokens
+    #Convert the tokens to your radical (root word) using tokens POS-tag before lemmatizing.
+    def lemmatizer(self, tokens):
+        wnl = WordNetLemmatizer()
 
-token_list = [tokenize_text(text) for text in corpus]
+        def POS_tag(token):
+            tag = pos_tag(token)
+            wntag = tag[0].lower()
+            wntag = wntag if wntag in ['a', 'r', 'n', 'v'] else None
+            return wntag
 
-pprint(token_list)
+        #for token, tag in pos_tag(tokens):
+        #    wntag = tag[0].lower()
+        #    wntag = wntag if wntag in ['a', 'r', 'n', 'v'] else None
+            #lemma = wnl.lemmatize(token, wntag) if wntag else token
+
+        lemma_tokens = [wnl.lemmatize(token, self.get_pos(token)) for token in tokens]
+
+        #lemma_tokens = [wnl.lemmatize(token, POS_tag(token)) if POS_tag(token) else token for token in tokens]
+        return lemma_tokens
+
+exem = exemplo()
+exem.token_list = [exem.tokenize_text(text) for text in exem.corpus]
+
+pprint(exem.token_list)
 
 #filtered_list_1 = [[remove_characters_after_tokenization(tokens)
 #                                for tokens in sentence_tokens]
 #                                    for sentence_tokens in token_list]
 
 #Loop to remove special characters from the list token_list.
-for sentence_tokens in token_list:
+for sentence_tokens in exem.token_list:
     for tokens in sentence_tokens:
-        filtered_list.append(list(filter(None, [remove_characters_after_tokenization(tokens)])))
+        exem.filtered_list.append(list(filter(None, [exem.remove_characters_after_tokenization(tokens)])))
 #Print list after remove special characters.
 #print(filtered_list)
 
 #Loop to convert words in filtered_list to lowercase.
-for sentence_tokens in filtered_list:
+for sentence_tokens in exem.filtered_list:
     for tokens in sentence_tokens:
-        filtered_list_lowercase.append(list(filter(None, [lower_case(tokens)])))
+        exem.filtered_list_lowercase.append(list(filter(None, [exem.lower_case(tokens)])))
 #Print list after convert to lowercase.
 #print(filtered_list_lowercase)
 
 #Loop to remove the stopwords from the list filtered_list_lowercase
-for sentence_tokens in filtered_list_lowercase:
+for sentence_tokens in exem.filtered_list_lowercase:
     for tokens in sentence_tokens:
-        filtered_list_remove_stopwords.append(list(filter(None, [remove_stopwords(tokens)])))
+        exem.filtered_list_remove_stopwords.append(list(filter(None, [exem.remove_stopwords(tokens)])))
 
 #Print list after remove stopwords
 #print (filtered_list_remove_stopwords)
 
 #Loop to remove the repeated characters from the list filtered_list_remove_stopwords.
-for sentence_tokens in filtered_list_remove_stopwords:
+for sentence_tokens in exem.filtered_list_remove_stopwords:
     for tokens in sentence_tokens:
-        filtered_list_remove_repeated_characters.append(list(filter(None, [remove_repeated_characters(tokens)])))
+        exem.filtered_list_remove_repeated_characters.append(list(filter(None, [exem.remove_repeated_characters(tokens)])))
 #Print list after remove repeated characters.
-print(filtered_list_remove_repeated_characters)
+print(exem.filtered_list_remove_repeated_characters)
 
 #Loop to stemming.
-for sentence_tokens in filtered_list_remove_repeated_characters:
+for sentence_tokens in exem.filtered_list_remove_repeated_characters:
     for tokens in sentence_tokens:
-        filtered_list_stemmer.append(list(filter(None, [lancaster_stemmer(tokens)])))
+        exem.filtered_list_stemmer.append(list(filter(None, [exem.lancaster_stemmer(tokens)])))
 #Print list after stemming.
-print(filtered_list_stemmer)
+print(exem.filtered_list_stemmer)
 
-for sentence_tokens in filtered_list_remove_repeated_characters:
+for sentence_tokens in exem.filtered_list_remove_repeated_characters:
     for tokens in sentence_tokens:
-        filtered_list_lemma.append(list(filter(None, [lemmatizer(tokens)])))
+        exem.filtered_list_lemma.append(list(filter(None, [exem.lemmatizer(tokens)])))
 
-print(filtered_list_lemma)
+print(exem.filtered_list_lemma)
--- text-classification-embedding.py
+++ text-classification-embedding.py
@@ -14,177 +14,183 @@ from nltk import pos_tag
 
 from collections import Counter
 
-#train = file_reader = csv.reader(open(data_path, "rt", errors="ignore", encoding="utf-8"), delimiter=',')
-data_path = "/home/paulo/PycharmProjects/suggestion-mining/training-full-v13-bkp.csv"
-#train = pd.read_csv(data_path, header=0, sep=";", quotechar='"',quoting=3, encoding='utf-8')
+class text_classification(object):
+    def __init__(self):
+        #train = file_reader = csv.reader(open(data_path, "rt", errors="ignore", encoding="utf-8"), delimiter=',')
+        self.data_path = "/home/paulo/PycharmProjects/suggestion-mining/training-full-v13-bkp.csv"
+        #train = pd.read_csv(data_path, header=0, sep=";", quotechar='"',quoting=3, encoding='utf-8')
 
-filtered_list = []
-filtered_list_lowercase = []
-filtered_list_remove_stopwords = []
-filtered_list_remove_repeated_characters = []
-filtered_list_stemmer = []
-filtered_list_lemma = []
+        self.sent_list = []
+        self.token_list = []
+        self.filtered_list = []
+        self.filtered_list_lowercase = []
+        self.filtered_list_remove_stopwords = []
+        self.filtered_list_remove_repeated_characters = []
+        self.filtered_list_stemmer = []
+        self.filtered_list_lemma = []
 
-# Reads a given CSV and stores the data in a list
-def read_csv(data_path):
-    file_reader = csv.reader(open(data_path, "rt", errors="ignore", encoding="utf-8"), delimiter=',')
-    sent_list = []
-    #print(file_reader.shape)
-    #print(file_reader.columns.values)
-    for row in file_reader:
-        id = row[0]
-        suggest = row[1]
-        sent = row[2]
-        sent_list.append((id, suggest, sent))
-    return sent_list
+    # Reads a given CSV and stores the data in a list
+    def read_csv(self, data_path):
+        file_reader = csv.reader(open(data_path, "rt", errors="ignore", encoding="utf-8"), delimiter=',')
+        sent_list = []
+        #print(file_reader.shape)
+        #print(file_reader.columns.values)
+        for row in file_reader:
+            id = row[0]
+            suggest = row[1]
+            sent = row[2]
+            sent_list.append((id, suggest, sent))
+        return sent_list
 
-#Tokenize the text into sentences
-def sent_tokenize(sent_list):
-    default_st = nltk.sent_tokenize
-    for sent in sent_list:
-        sentence = default_st(text=sent[1])
-        print(sentence)
+    #Tokenize the text into sentences
+    def sent_tokenize(self, sent_list):
+        default_st = nltk.sent_tokenize
+        for sent in sent_list:
+            sentence = default_st(text=sent[1])
+            print(sentence)
 
-def word_tokenize1(sent_list):
-    default_w = nltk.word_tokenize
-    for sent in sent_list:
-        token_list = [default_w(sent[1])]
-        print(token_list[0])
+    def word_tokenize1(self, sent_list):
+        default_w = nltk.word_tokenize
+        for sent in sent_list:
+            token_list = [default_w(sent[1])]
+            print(token_list[0])
 
-#Tokenize the sentence into words.
-def word_tokenize(sentence):
-    default_w = nltk.word_tokenize
-    word_tokens = [default_w(sentence)]
-    return word_tokens
+    #Tokenize the sentence into words.
+    def word_tokenize(self, sentence):
+        default_w = nltk.word_tokenize
+        word_tokens = [default_w(sentence)]
+        return word_tokens
 
-# Remove special characters after tokenization
-def remove_characters_after_tokenization(token_list):
-    pattern = re.compile('[{}]'.format(re.escape(string.punctuation)))
-    #for token in token_list:
-    #    filtered_tokens = list(filter(None, [pattern.sub('', token) ]))
+    # Remove special characters after tokenization
+    def remove_characters_after_tokenization(self, token_list):
+        pattern = re.compile('[{}]'.format(re.escape(string.punctuation)))
+        #for token in token_list:
+        #    filtered_tokens = list(filter(None, [pattern.sub('', token) ]))
 
-#    for tokens in token_list:
-#        for token in tokens:
-#            filtered_tokens = list([filter(None, [pattern.sub('', token)])])
-    filtered_tokens = list(filter(None, [pattern.sub('', token) for token in token_list]))
-    #print(filtered_tokens)
+    #    for tokens in token_list:
+    #        for token in tokens:
+    #            filtered_tokens = list([filter(None, [pattern.sub('', token)])])
+        filtered_tokens = list(filter(None, [pattern.sub('', token) for token in token_list]))
+        #print(filtered_tokens)
 
-    return filtered_tokens
+        return filtered_tokens
 
-#Convert tokens to lowercase.
-def lower_case(token_list):
-    lower_token_list = [token.lower() for token in token_list]
-    return lower_token_list
+    #Convert tokens to lowercase.
+    def lower_case(self, token_list):
+        lower_token_list = [token.lower() for token in token_list]
+        return lower_token_list
 
-#Remove the stopwords from the token list.
-def remove_stopwords(tokens):
-    stopword_list = nltk.corpus.stopwords.words('english')
-    filtered_tokens = [token for token in tokens if token not in stopword_list]
-    return filtered_tokens
+    #Remove the stopwords from the token list.
+    def remove_stopwords(self, tokens):
+        stopword_list = nltk.corpus.stopwords.words('english')
+        filtered_tokens = [token for token in tokens if token not in stopword_list]
+        return filtered_tokens
 
-#Remove the repeated characters from the token list.
-def remove_repeated_characters(tokens):
-    repeat_pattern = re.compile(r'(\w*)(\w)\2(\w*)')
-    match_substitution = r'\1\2\3'
-    def replace(old_word):
-        if wordnet.synsets(old_word):
-            return old_word
+    #Remove the repeated characters from the token list.
+    #Identify repeated characters in a word using a regex pattern and then use a substitution to remove the characters one by one.
+    def remove_repeated_characters(self, tokens):
+        repeat_pattern = re.compile(r'(\w*)(\w)\2(\w*)')
+        match_substitution = r'\1\2\3'
+        def replace(old_word):
+            if wordnet.synsets(old_word):
+                return old_word
 
-        new_word = repeat_pattern.sub(match_substitution, old_word)
-        return replace(new_word) if new_word != old_word else new_word
+            new_word = repeat_pattern.sub(match_substitution, old_word)
+            return replace(new_word) if new_word != old_word else new_word
 
-    correct_tokens = [replace(word) for word in tokens]
-    return correct_tokens
+        correct_tokens = [replace(word) for word in tokens]
+        return correct_tokens
 
-#Stemming the tokens.
-def lancaster_stemmer(tokens):
-    ls = LancasterStemmer()
-    filtered_tokens = [ls.stem(token) for token in tokens]
-    return filtered_tokens
+    #Stemming the tokens.
+    def lancaster_stemmer(self, tokens):
+        ls = LancasterStemmer()
+        filtered_tokens = [ls.stem(token) for token in tokens]
+        return filtered_tokens
 
-#POS-tag token
-def get_pos(word):
-    w_synsets = wordnet.synsets(word)
+    #POS-tag token
+    def get_pos(self, word):
+        w_synsets = wordnet.synsets(word)
 
-    pos_counts = Counter()
-    pos_counts["n"] = len([item for item in w_synsets if item.pos() == "n"])
-    pos_counts["v"] = len([item for item in w_synsets if item.pos() == "v"])
-    pos_counts["a"] = len([item for item in w_synsets if item.pos() == "a"])
-    pos_counts["r"] = len([item for item in w_synsets if item.pos() == "r"])
+        pos_counts = Counter()
+        pos_counts["n"] = len([item for item in w_synsets if item.pos() == "n"])
+        pos_counts["v"] = len([item for item in w_synsets if item.pos() == "v"])
+        pos_counts["a"] = len([item for item in w_synsets if item.pos() == "a"])
+        pos_counts["r"] = len([item for item in w_synsets if item.pos() == "r"])
 
-    most_common_pos_list = pos_counts.most_common(3)
-    return most_common_pos_list[0][0]  # first indexer for getting the top POS from list, second indexer for getting POS from tuple( POS: count )
+        most_common_pos_list = pos_counts.most_common(3)
+        return most_common_pos_list[0][0]  # first indexer for getting the top POS from list, second indexer for getting POS from tuple( POS: count )
 
 
-#Convert the tokens to your radical (root word) using tokens POS-tag before lemmatizing.
-def lemmatizer(tokens):
-    wnl = WordNetLemmatizer()
+    #Convert the tokens to your radical (root word) using tokens POS-tag before lemmatizing.
+    def lemmatizer(self, tokens):
+        wnl = WordNetLemmatizer()
 
-    def POS_tag(token):
-        tag = pos_tag(token)
-        wntag = tag[0].lower()
-        wntag = wntag if wntag in ['a', 'r', 'n', 'v'] else None
-        return wntag
+        def POS_tag(token):
+            tag = pos_tag(token)
+            wntag = tag[0].lower()
+            wntag = wntag if wntag in ['a', 'r', 'n', 'v'] else None
+            return wntag
 
-    #for token, tag in pos_tag(tokens):
-    #    wntag = tag[0].lower()
-    #    wntag = wntag if wntag in ['a', 'r', 'n', 'v'] else None
-        #lemma = wnl.lemmatize(token, wntag) if wntag else token
+        #for token, tag in pos_tag(tokens):
+        #    wntag = tag[0].lower()
+        #    wntag = wntag if wntag in ['a', 'r', 'n', 'v'] else None
+            #lemma = wnl.lemmatize(token, wntag) if wntag else token
 
-    lemma_tokens = [wnl.lemmatize(token, get_pos(token)) for token in tokens]
+        lemma_tokens = [wnl.lemmatize(token, self.get_pos(token)) for token in tokens]
 
-    #lemma_tokens = [wnl.lemmatize(token, POS_tag(token)) if POS_tag(token) else token for token in tokens]
-    return lemma_tokens
+        #lemma_tokens = [wnl.lemmatize(token, POS_tag(token)) if POS_tag(token) else token for token in tokens]
+        return lemma_tokens
 
-sent_list = read_csv(data_path)
+classifier = text_classification()
+classifier.sent_list = classifier.read_csv(classifier.data_path)
 #sent_tokenize(sent_list)
 #word_tokenize(sent_list)
-token_list = [word_tokenize(sent[1]) for sent in sent_list]
+classifier.token_list = [classifier.word_tokenize(sent[1]) for sent in classifier.sent_list]
 #Print list after execute word tokenize.
 #pprint(token_list)
 
 #Loop to remove special characters from the list token_list.
-for sentence_tokens in token_list:
+for sentence_tokens in classifier.token_list:
     for tokens in sentence_tokens:
-        filtered_list.append(list(filter(None, [remove_characters_after_tokenization(tokens)])))
+        classifier.filtered_list.append(list(filter(None, [classifier.remove_characters_after_tokenization(tokens)])))
 #Print list after remove special characters.
 #print(filtered_list)
 
 #Loop to convert words in filtered_list to lowercase.
-for sentence_tokens in filtered_list:
+for sentence_tokens in classifier.filtered_list:
     for tokens in sentence_tokens:
-        filtered_list_lowercase.append(list(filter(None, [lower_case(tokens)])))
+        classifier.filtered_list_lowercase.append(list(filter(None, [classifier.lower_case(tokens)])))
 #Print list after convert to lowercase.
 #print(filtered_list_lowercase)
 
 #Loop to remove the stopwords from the list filtered_list_lowercase
-for sentence_tokens in filtered_list_lowercase:
+for sentence_tokens in classifier.filtered_list_lowercase:
     for tokens in sentence_tokens:
-        filtered_list_remove_stopwords.append(list(filter(None, [remove_stopwords(tokens)])))
+        classifier.filtered_list_remove_stopwords.append(list(filter(None, [classifier.remove_stopwords(tokens)])))
 
 #Print list after remove stopwords
 #print (filtered_list_remove_stopwords)
 
 #Loop to remove the repeated characters from the list filtered_list_remove_stopwords.
-for sentence_tokens in filtered_list_remove_stopwords:
+for sentence_tokens in classifier.filtered_list_remove_stopwords:
     for tokens in sentence_tokens:
-        filtered_list_remove_repeated_characters.append(list(filter(None, [remove_repeated_characters(tokens)])))
+        classifier.filtered_list_remove_repeated_characters.append(list(filter(None, [classifier.remove_repeated_characters(tokens)])))
 #Print list after remove repeated characters.
-for sentence_tokens in filtered_list_remove_repeated_characters:
+for sentence_tokens in classifier.filtered_list_remove_repeated_characters:
     for tokens in sentence_tokens:
       print(tokens)
 
 #Loop to stemming.
-for sentence_tokens in filtered_list_remove_repeated_characters:
+for sentence_tokens in classifier.filtered_list_remove_repeated_characters:
     for tokens in sentence_tokens:
-        filtered_list_stemmer.append(list(filter(None, [lancaster_stemmer(tokens)])))
+        classifier.filtered_list_stemmer.append(list(filter(None, [classifier.lancaster_stemmer(tokens)])))
 #Print list after stemming.
 #print(filtered_list_stemmer)
 
 
 
-for sentence_tokens in filtered_list_remove_repeated_characters:
+for sentence_tokens in classifier.filtered_list_remove_repeated_characters:
     for tokens in sentence_tokens:
-        filtered_list_lemma.append(list(filter(None, [lemmatizer(tokens)])))
+        classifier.filtered_list_lemma.append(list(filter(None, [classifier.lemmatizer(tokens)])))
 
-print(filtered_list_lemma)
+print(classifier.filtered_list_lemma)
